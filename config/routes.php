8f8caea304c5ceb066939c4b30b7065ba41c9852
Alexander <shevkunov.alexander@gmail.com>
2018-01-22 18:05:26 +0200

    Add files via upload

---
+++ config/routes.php
@@ -0,0 +1,45 @@
+<?php
+return array(
+
+	'product/([0-9]+)' => 'product/view/$1',
+
+	'catalog' => 'catalog/index',
+
+	'category/([0-9]+)/page-([0-9]+)' => 'catalog/category/$1/$2',
+	'category/([0-9]+)' => 'catalog/category/$1',
+
+	'cart/checkout' => 'cart/checkout',
+	'cart/delete/([0-9]+)' => 'cart/delete',
+	'cart/add/([0-9]+)' => 'cart/add/$1',
+	'cart/addAjax/([0-9]+)' => 'cart/addAjax/$1',
+	'cart' => 'cart/index',
+
+	'user/register' => 'user/register',
+	'user/login' => 'user/login',
+	'user/logout' => 'user/logout',
+
+	'cabinet/edit' => 'cabinet/edit',
+	'cabinet' => 'cabinet/index',
+
+	'admin/product/create' => 'adminProduct/create',
+	'admin/product/update/([0-9]+)' => 'adminProduct/update/$1',
+	'admin/product/delete/([0-9]+)' => 'adminProduct/delete/$1',
+	'admin/product' => 'adminProduct/index',
+
+	'admin/category/create' => 'adminCategory/create',
+	'admin/category/update/([0-9]+)' => 'adminCategory/update/$1',
+	'admin/category/delete/([0-9]+)' => 'adminCategory/delete/$1',
+	'admin/category' => 'adminCategory/index',
+
+	'admin/order/update/([0-9]+)' => 'adminOrder/update/$1',
+	'admin/order/delete/([0-9]+)' => 'adminOrder/delete/$1',
+	'admin/order/view/([0-9]+)' => 'adminOrder/view/$1',
+	'admin/order' => 'adminOrder/index',
+    
+	'admin' => 'admin/index',
+
+	'contacts' => 'site/contact',
+
+	'' => 'site/index',
+
+);
