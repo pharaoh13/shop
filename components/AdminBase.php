88a0214b82e4f121bbf67f4e05f672dc95617284
Alexander <shevkunov.alexander@gmail.com>
2018-01-22 18:04:25 +0200

    Add files via upload

---
+++ components/AdminBase.php
@@ -0,0 +1,21 @@
+<?php 
+
+abstract class AdminBase
+{
+	public static function checkAdmin()
+	{
+		// Проверяем авторизирован ли пользователь. Если нет, то он будет переадресован
+		$userId = User::checkLogged();
+
+		// Получаем информацию о текущем пользователе
+		$user = User::getUserById($userId);
+
+		// Если роль текущего пользователя "admin", пускаем его в админпанель
+		if ($user['role'] == 'admin') {
+			return true;
+		}
+
+		// Иначе завершаем работу с сообщением о закрытом доступе
+		die('Access denied');
+	}
+}
